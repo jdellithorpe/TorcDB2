4b7d32c9f7e204722ccf32f827b5699aea133a22
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-09-09 13:09:20 -0600

    Added 'traverse' test to PerfUtil.
    This test measures the performance of the Graph.traverse() method. The
    test performs a parameter sweep over the size of the starting set of
    nodes and the edge degree of those nodes. Currently all edges go to
    unique neighbors.

--- src/main/java/net/ellitron/torcdb2/util/PerfUtil.java
+++ src/main/java/net/ellitron/torcdb2/util/PerfUtil.java
@@ -22,27 +22,34 @@ import org.docopt.Docopt;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import java.lang.Math;
+import java.lang.reflect.InvocationTargetException;
+
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+
 import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * A utility for running microbenchmarks on TorcDB2.
@@ -55,6 +62,7 @@ public class PerfUtil {
       + "\n"
       + "Usage:\n"
       + "  PerfUtil [options] edge_rdwr\n"
+      + "  PerfUtil [options] traverse\n"
       + "  PerfUtil (-h | --help)\n"
       + "  PerfUtil --version\n"
       + "\n"
@@ -217,6 +225,147 @@ public class PerfUtil {
                 writeLatency[(int) (0.50 * (float) writeLatency.length)]/1000, 
                 readLatency[(int) (0.50 * (float) readLatency.length)]/1000));
         }
+      } else if ((Boolean) opts.get("traverse")) {
+        Graph graph = new Graph(config);
+
+        long nodes_start = Long.decode(config.get("traverse.nodes.start"));
+        long nodes_end = Long.decode(config.get("traverse.nodes.end"));
+        long nodes_points = Long.decode(config.get("traverse.nodes.points"));
+        String nodes_mode = config.get("traverse.nodes.mode");
+
+        long degree_start = Long.decode(config.get("traverse.degree.start"));
+        long degree_end = Long.decode(config.get("traverse.degree.end"));
+        long degree_points = Long.decode(config.get("traverse.degree.points"));
+        String degree_mode = config.get("traverse.degree.mode");
+
+        long samples = Long.decode(config.get("traverse.samples"));
+       
+
+        List<Long> nodes_params = new ArrayList<>();
+        if (nodes_points > 1)
+          if (nodes_mode.equals("linear"))
+            for ( long i = nodes_start; 
+                  i <= nodes_end; 
+                  i += (nodes_end - nodes_start) / (nodes_points - 1) )
+              nodes_params.add(i);    
+          else if (nodes_mode.equals("geometric")) {
+            double c = Math.pow(10, Math.log10((double)nodes_end/(double)nodes_start) 
+                                                / (double)(nodes_points - 1));
+            for (long i = nodes_start; i <= nodes_end; i = (long)Math.ceil(c * i))
+              nodes_params.add(i);
+          } else
+            throw new RuntimeException(
+                "Error: Unrecognized value for parameter traverse.nodes.mode");
+        else 
+          nodes_params.add(nodes_start);
+
+        List<Long> degree_params = new ArrayList<>(); 
+        if (degree_points > 1)
+          if (degree_mode.equals("linear"))
+            for ( long i = degree_start; 
+                  i <= degree_end; 
+                  i += (degree_end - degree_start) / (degree_points - 1) )
+              degree_params.add(i);    
+          else if (degree_mode.equals("geometric")) {
+            double c = Math.pow(10, Math.log10((double)degree_end/(double)degree_start) 
+                                                / (double)(degree_points - 1));
+            for (long i = degree_start; i <= degree_end; i = (long)Math.ceil(c * i))
+              degree_params.add(i);
+          } else
+            throw new RuntimeException(
+                "Error: Unrecognized value for parameter traverse.degree.mode");
+        else 
+          degree_params.add(degree_start);
+
+        // WarmUp
+        // ...
+
+        // Output the header
+        // degree, nodes, min, mean, max, 50th, 90th, 95th, 99th, 99.9th
+        System.out.println("degree, nodes, min, mean, max, 50th, 90th, 95th, 99th, 99.9th");
+
+        long degree_prev = 0;
+        for (long degree : degree_params) {
+          // Experiment setup: For each degree we setup all the nodes to have that degree. Each time
+          // degree is increased, we add the additional edges needed to the existing nodes to bring
+          // the degree up to the specified amount.
+          for (long i = 0; i < nodes_end; i++) {
+            Vertex startNode = new Vertex(new UInt128(0,i), "Person");
+            for (long j = degree_prev; j < degree; j++) {
+              // Vertices need labels, need to create vertex objects
+              graph.addEdge(startNode, 
+                            "knows", 
+                            new Vertex(new UInt128(0, nodes_end + j*nodes_end + i), "Person"), 
+                            null);
+            }
+          }
+
+          long nodes_prev = 0;
+          Set<Vertex> startNodeSet = new HashSet<>();
+          for (long nodes : nodes_params) {
+            for (long i = nodes_prev; i < nodes; i++)
+              startNodeSet.add(new Vertex(new UInt128(0,i), "Person"));
+
+            // Execution times are recorded in nanoseconds.
+            Long[] execTimes = new Long[(int)samples];
+            for (long i = 0; i < samples; i++) {
+              long startTime = System.nanoTime();
+              TraversalResult result = graph.traverse(startNodeSet, 
+                                                      "knows", 
+                                                      Direction.OUT, 
+                                                      false, 
+                                                      "Person");
+              long endTime = System.nanoTime();
+              execTimes[(int)i] = endTime - startTime;
+            }
+
+            // Calculate latency statistics
+            Arrays.sort(execTimes);
+
+            long sum = 0;
+            long min = Long.MAX_VALUE;
+            long max = 0;
+            for (int k = 0; k < execTimes.length; k++) {
+              sum += execTimes[k];
+
+              if (execTimes[k] < min)
+                min = execTimes[k];
+
+              if (execTimes[k] > max)
+                max = execTimes[k];
+            }
+
+            long mean = sum / execTimes.length;
+
+            long p25  = (int) (0.250 * (float) execTimes.length);
+            long p50  = (int) (0.500 * (float) execTimes.length);
+            long p75  = (int) (0.750 * (float) execTimes.length);
+            long p90  = (int) (0.900 * (float) execTimes.length);
+            long p95  = (int) (0.950 * (float) execTimes.length);
+            long p99  = (int) (0.990 * (float) execTimes.length);
+            long p999 = (int) (0.999 * (float) execTimes.length);
+
+            // Print out the results (in microseconds)
+            // degree, nodes, min, mean, max, 50th, 90th, 95th, 99th, 99.9th
+            System.out.println(String.format("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d", 
+                                              degree,
+                                              nodes,
+                                              min/1000,
+                                              mean/1000,
+                                              max/1000,
+                                              execTimes[(int)p50]/1000,
+                                              execTimes[(int)p90]/1000,
+                                              execTimes[(int)p95]/1000,
+                                              execTimes[(int)p99]/1000,
+                                              execTimes[(int)p999]/1000));
+              
+            nodes_prev = nodes;
+          }
+
+          degree_prev = degree;
+        }
+
+        graph.close();
       }
     }
   }
