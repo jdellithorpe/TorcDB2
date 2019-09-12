0732630a8465cd8a377c8be7730d02df9c0ddc16
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-09-11 22:53:42 -0600

    Turned off debug print in EdgeList.

--- src/main/java/net/ellitron/torcdb2/EdgeList.java
+++ src/main/java/net/ellitron/torcdb2/EdgeList.java
@@ -819,40 +819,40 @@ public class EdgeList {
     if (totalLockWaitTime == 0)
       totalLockWaitTime = totalReadTime;
 
-    System.out.println(String.format(
-          "{\"tag\": \"EdgeList.batchReadThread()\", "
-          + "\"numThreads\": %d, "
-          + "\"threadId\": %d, "
-          + "\"totalRequests\": %d, "
-          + "\"totalHeadSegments\": %d, "
-          + "\"totalTailSegments\": %d, "
-          + "\"totalReadTime\": %d, "
-          + "\"totalLockWaitTime\": %d, "
-          + "\"totalDeserializationTime\": %d, "
-          + "\"totalEdges\": %d, "
-          + "\"parseProps\": %s, "
-          + "\"avgDeserTimePerEdge\": %.3f, "
-          + "\"avgDeserTimePerSeg\": %.3f, "
-          + "\"totalTime\": %d, "
-          + "\"percentTimeRead\": %.2f, "
-          + "\"percentTimeLockWait\": %.2f, "
-          + "\"percentTimeDeser\": %.2f}",
-          numThreads,
-          threadId,
-          totalRequests,
-          totalHeadSegments,
-          totalTailSegments,
-          totalReadTime/1000,
-          (totalLockWaitTime/1000) - (totalReadTime/1000),
-          totalDeserializationTime/1000,
-          totalEdges,
-          parseProps,
-          (double)totalDeserializationTime/(double)totalEdges/1000.0,
-          (double)totalDeserializationTime/(double)totalRequests/1000.0,
-          (endTime - startTime)/1000,
-          (double)totalReadTime/(double)(endTime - startTime),
-          (double)(totalLockWaitTime - totalReadTime)/(double)(endTime - startTime),
-          (double)totalDeserializationTime/(double)(endTime - startTime)));
+//    System.out.println(String.format(
+//          "{\"tag\": \"EdgeList.batchReadThread()\", "
+//          + "\"numThreads\": %d, "
+//          + "\"threadId\": %d, "
+//          + "\"totalRequests\": %d, "
+//          + "\"totalHeadSegments\": %d, "
+//          + "\"totalTailSegments\": %d, "
+//          + "\"totalReadTime\": %d, "
+//          + "\"totalLockWaitTime\": %d, "
+//          + "\"totalDeserializationTime\": %d, "
+//          + "\"totalEdges\": %d, "
+//          + "\"parseProps\": %s, "
+//          + "\"avgDeserTimePerEdge\": %.3f, "
+//          + "\"avgDeserTimePerSeg\": %.3f, "
+//          + "\"totalTime\": %d, "
+//          + "\"percentTimeRead\": %.2f, "
+//          + "\"percentTimeLockWait\": %.2f, "
+//          + "\"percentTimeDeser\": %.2f}",
+//          numThreads,
+//          threadId,
+//          totalRequests,
+//          totalHeadSegments,
+//          totalTailSegments,
+//          totalReadTime/1000,
+//          (totalLockWaitTime/1000) - (totalReadTime/1000),
+//          totalDeserializationTime/1000,
+//          totalEdges,
+//          parseProps,
+//          (double)totalDeserializationTime/(double)totalEdges/1000.0,
+//          (double)totalDeserializationTime/(double)totalRequests/1000.0,
+//          (endTime - startTime)/1000,
+//          (double)totalReadTime/(double)(endTime - startTime),
+//          (double)(totalLockWaitTime - totalReadTime)/(double)(endTime - startTime),
+//          (double)totalDeserializationTime/(double)(endTime - startTime)));
 
     return new TraversalResult(vMap, pMap, vSet);
   }
